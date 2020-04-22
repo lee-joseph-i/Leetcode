ed96892ace0b348c91838c3ceae9eb5cac060688
Joseph Lee <josephinlee91@gmail.com>
2020-04-22 12:16:15 -0700

    solve getTargetCopy

--- leetcode.js
+++ leetcode.js
@@ -232,3 +232,12 @@ function getHeight(arr) {
   }
   return max
 }
+
+//getTargetCopy (medium)
+var getTargetCopy = function (original, cloned, target) {
+  if (!cloned) return null;
+
+  if (cloned.val === target.val) return cloned;
+
+  return getTargetCopy(original, cloned.left, target) || getTargetCopy(original, cloned.right, target)
+};

1b9df96cf755bb65c4edb67b62efa8d95bc1b8b7
Joseph Lee <josephinlee91@gmail.com>
2020-07-07 18:58:35 -0700

    solve islandPerimeter

--- leetcode.js
+++ leetcode.js
@@ -1405,3 +1405,18 @@ function favoriteRestaurant(requests, restaurants) {
     // - both constraints are helpful to determine efficiency-necessity. These are fairly low data inputs that even a   //quadratic runtime will likely not impact user experience.
   }
 };
+
+var islandPerimeter = function (grid) {
+  let perimeter = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        if (grid[i - 1]?.[j] !== 1) perimeter++;
+        if (grid[i + 1]?.[j] !== 1) perimeter++;
+        if (grid[i][j - 1] !== 1) perimeter++;
+        if (grid[i][j + 1] !== 1) perimeter++;
+      };
+    };
+  };
+  return perimeter;
+};

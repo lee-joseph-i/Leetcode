11d3a8afb52dfaf337e6976dca1ebd6a012add0d
Joseph Lee <josephinlee91@gmail.com>
2020-07-17 23:43:42 -0700

    solve topKFrequent

--- leetcode.js
+++ leetcode.js
@@ -1645,3 +1645,34 @@ var myPow = function (x, n) {
   }
   return n < 0 ? 1 / result : result;
 };
+
+var topKFrequent = function (nums, k) {
+  const n = nums.length;
+  const freq = new Map();
+
+  for (const num of nums) {
+    if (!freq.get(num)) freq.set(num, 0);
+    freq.set(num, freq.get(num) + 1);
+  }
+
+  const buckets = [];
+
+  for (let i = 0; i <= n; i++) {
+    buckets[i] = [];
+  }
+
+  for (const [num, count] of freq) {
+    buckets[count].push(num);
+  }
+
+  const res = [];
+
+  for (let i = n; i >= 0 && k > 0; i--) {
+    if (buckets[i].length > 0) {
+      k -= buckets[i].length;
+      res.push(...buckets[i]);
+    }
+  }
+
+  return res;
+};

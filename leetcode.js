ca6628bf093baaad0840b40e4d454d59b895c5ca
Joseph Lee <josephinlee91@gmail.com>
2020-07-20 23:17:58 -0700

    way more intuitive solution of removeElements

--- leetcode.js
+++ leetcode.js
@@ -1727,3 +1727,20 @@ var removeElements = function (head, val) {
 
   return head;
 };
+
+var removeElements = function (head, val) {
+  while (head && head.val === val) {
+    head = head.next;
+  };
+
+  let node = head;
+  while (node && node.next) {
+    if (node.next.val === val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    };
+  };
+
+  return head;
+};

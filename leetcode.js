fd05103bbc575b5ee388a5e6913c56703502148e
Joseph Lee <josephinlee91@gmail.com>
2020-07-22 23:20:04 -0700

    solve zigzaglevelorder

--- leetcode.js
+++ leetcode.js
@@ -1768,3 +1768,31 @@ const buildWord = (board, word, x, y, currentWord = "") => {
   if (top || right || bottom || left) return true;
   board[y][x] = char;
 };
+
+var zigzagLevelOrder = function (root) {
+  if (!root) {
+    return [];
+  } else {
+    const traversed = [[root.val]];
+    let parents = [root];
+    let children = [];
+    let zigZag = false;
+    while (parents.length > 0) {
+      for (let root of parents) {
+        if (root.left)
+          children.push(root.left);
+        if (root.right)
+          children.push(root.right);
+      }
+      if (children.length > 0) {
+        traversed.push(
+          zigZag ? children.map(child => child.val) : children.map(child => child.val).reverse()
+        );
+      }
+      zigZag = !zigZag;
+      parents = [...children];
+      children = [];
+    }
+    return traversed;
+  }
+};

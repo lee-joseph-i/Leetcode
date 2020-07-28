5852ec70cc63ffc3d97918de27c3cf9000dd49bd
Joseph Lee <josephinlee91@gmail.com>
2020-07-27 22:12:34 -0700

    solve buildTree

--- leetcode.js
+++ leetcode.js
@@ -1851,3 +1851,14 @@ var addDigits = function (num) {
 
   return result;
 };
+
+var buildTree = function (inorder, postorder) {
+  if (inorder.length === 0) return null;
+  let root = postorder[postorder.length - 1];
+  let pivot = inorder.indexOf(root);
+  return {
+    val: root,
+    left: buildTree(inorder.slice(0, pivot), postorder.slice(0, pivot)),
+    right: buildTree(inorder.slice(pivot + 1), postorder.slice(pivot, -1))
+  };
+};

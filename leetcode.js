9e8c56094b90b13a6ba41809b4a1f7efbbbb8e88
Joseph Lee <josephinlee91@gmail.com>
2020-08-20 22:57:06 -0700

    solve longestPalindrome

--- leetcode.js
+++ leetcode.js
@@ -1901,3 +1901,20 @@ var isPowerOfFour = function (num) {
   };
   return false;
 };
+
+var longestPalindrome = function(s) {
+    const set = new Set();
+    let count = 0;
+    
+    for (let char of s) {
+        if (set.has(char)) {
+			count += 2;
+            set.delete(char);
+        } 
+		else {
+            set.add(char);
+        }
+    }
+
+    return count + (set.size > 0 ? 1 : 0);
+};

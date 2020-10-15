e2380a36d9db70ef5862577978e2b1ee4500bc5a
Joseph Lee <josephinlee91@gmail.com>
2020-10-14 19:28:24 -0700

    annotate tricky parts

--- increasing-decreasing-string.js
+++ increasing-decreasing-string.js
@@ -1,25 +1,23 @@
 var sortString = function (s) {
   let result = "";
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
   let sorted = s.split("").sort();
   let current = "";
   while (sorted.length) {
     current = "";
-    //step 1
     let smallestChar = sorted.splice(0, 1)[0];
     result += smallestChar;
 
-    //step 2
     for (let i = 0; i < sorted.length; i++) {
       if (sorted[i] !== smallestChar) {
         current = sorted.splice(i, 1)[0];
         result += current;
-        //step 3
         smallestChar = current;
-        i--;
+        i--; //when you splice, you're going to skip an index.
       }
     }
 
-    //step 4
     if (sorted.length === 0) break;
     let largestChar = sorted.splice(sorted.length - 1, 1)[0];
     result += largestChar;
@@ -27,13 +25,11 @@ var sortString = function (s) {
     //step 5
     for (let i = sorted.length - 1; i >= 0; i--) {
       if (sorted[i] !== largestChar) {
-        current = sorted.splice(i, 1)[0];
+        current = sorted.splice(i, 1)[0]; //when you splice BACKWARDS though, you don't have to i++; in fact, that would mess you up.
         result += current;
-        //step 6
         largestChar = current;
       }
     }
-    //step 7 conditional here
   }
   return result;
 };
